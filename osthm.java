d893bc77d193834a16dc5e2a7ff04178637fc2ff
Iyxan <31884435+Iyxan23@users.noreply.github.com>
2020-10-02 19:21:21 +0700

    Initialize osthm.java

--- osthm.java
+++ osthm.java
@@ -1,2 +1,19 @@
-// Being worked on
 // This java code must be injected into an app to work, or you might use os-thm-sketchware-asd.java if you can't inject it.
+
+
+
+public class osthm {
+    
+    public static final String externalDir = Environment.getExternalStorageDirectory().getAbsolutePath();
+    public static final String themes_folder = externalDir + "/.os-thm/themes/";
+    public static final String config_file = externalDir + "/.os-thm/conf";
+    public static final String exported_themes_folder = externalDir + "/os-thm/";
+  
+    public static Theme getCurrentTheme() {
+        
+    }
+    
+    public class Theme {
+        
+    }
+}
